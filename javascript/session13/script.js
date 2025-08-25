2f22082da73b095dde799e49f632af83c8a00541
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-08-25 20:01:28 +0330

    c

--- javascript/session13/script.js
+++ javascript/session13/script.js
@@ -1,25 +1,34 @@
-const addAnimationButton = document.querySelector('button')
-const div = document.querySelector('div')
-const p = document.querySelector('p')
-function setAnimation(){
-    div.style.animation = 'move 4s 3'
-}
-function animationStartHandler(event){
-    p.innerHTML = "Animation started"
-    p.style.backgroundColor = 'red'
-    div.style.backgroundColor = 'red'
-}
-function animationIterationHandler(event){
-    p.innerHTML = "Animation repeated"
-    p.style.backgroundColor = 'green'
-    div.style.backgroundColor = 'green'
-}
-function animationEndHandler(event){
-    p.innerHTML = "Animation ended"
-    p.style.backgroundColor = 'blue'
-    div.style.backgroundColor = 'blue'
-}
-addAnimationButton.addEventListener('click', setAnimation)
-div.addEventListener('animationstart', animationStartHandler)
-div.addEventListener('animationiteration', animationIterationHandler)
-div.addEventListener('animationend', animationEndHandler)
+// const addAnimationButton = document.querySelector('button')
+// const div = document.querySelector('div')
+// const p = document.querySelector('p')
+// function setAnimation(){
+//     div.style.animation = 'move 4s 3'
+// }
+// function animationStartHandler(event){
+//     p.innerHTML = "Animation started"
+//     p.style.backgroundColor = 'red'
+//     div.style.backgroundColor = 'red'
+// }
+// function animationIterationHandler(event){
+//     p.innerHTML = "Animation repeated"
+//     p.style.backgroundColor = 'green'
+//     div.style.backgroundColor = 'green'
+// }
+// function animationEndHandler(event){
+//     p.innerHTML = "Animation ended"
+//     p.style.backgroundColor = 'blue'
+//     div.style.backgroundColor = 'blue'
+// }
+// addAnimationButton.addEventListener('click', setAnimation)
+// div.addEventListener('animationstart', animationStartHandler)
+// div.addEventListener('animationiteration', animationIterationHandler)
+// div.addEventListener('animationend', animationEndHandler)
+
+
+
+const r = document.getElementById('range')
+const c = document.querySelector('.container')
+
+r.addEventListener('input', function(event){
+c.style.filter = 'brightness(' + event.target.value + '%)'
+})
