5d2da9831db941aba0e7410389b8e0ba6c87118c
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-08-25 19:46:58 +0330

    g

---
+++ javascript/session13/script.js
@@ -0,0 +1,25 @@
+const addAnimationButton = document.querySelector('button')
+const div = document.querySelector('div')
+const p = document.querySelector('p')
+function setAnimation(){
+    div.style.animation = 'move 4s 3'
+}
+function animationStartHandler(event){
+    p.innerHTML = "Animation started"
+    p.style.backgroundColor = 'red'
+    div.style.backgroundColor = 'red'
+}
+function animationIterationHandler(event){
+    p.innerHTML = "Animation repeated"
+    p.style.backgroundColor = 'green'
+    div.style.backgroundColor = 'green'
+}
+function animationEndHandler(event){
+    p.innerHTML = "Animation ended"
+    p.style.backgroundColor = 'blue'
+    div.style.backgroundColor = 'blue'
+}
+addAnimationButton.addEventListener('click', setAnimation)
+div.addEventListener('animationstart', animationStartHandler)
+div.addEventListener('animationiteration', animationIterationHandler)
+div.addEventListener('animationend', animationEndHandler)
