e39e47fcc08077795c991b305d0c77998ad83b1d
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-09-23 17:22:41 +0330

    d

--- database/schoolmanagement/student_management_form.py
+++ database/schoolmanagement/student_management_form.py
@@ -8,20 +8,39 @@ class StudentForm(QDialog):
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+        self.db = Database()
         self.load_students()
+        self.ui.pushButton_add_student.clicked.connect(self.add_student)
 
     def load_students(self):
-        db = Database()
-        cur = db.cursor
-        cur.execute("SELECT id, first_name, last_name,class_id FROM students ORDER BY id")
-        rows = cur.fetchall()
+        self.db.execute("SELECT id, first_name, last_name,class_id FROM students ORDER BY id")
+        rows = self.db.cursor.fetchall()
      
 
         self.ui.tableWidget_students.setRowCount(0)
+        self.ui.tableWidget_students.setColumnCount(len(dict(rows[0]).keys()))
+        self.ui.tableWidget_students.setHorizontalHeaderLabels(dict(rows[0]).keys())
+        
         for row_num, row in enumerate(rows):
             self.ui.tableWidget_students.insertRow(row_num)
            
+            col_index = 0
             for col_num, data in dict(row).items():
-                print(col_num, data)
-                self.ui.tableWidget_students.setItem(row_num, col_num, QTableWidgetItem(str(data)))
+                self.ui.tableWidget_students.setItem(row_num, col_index, QTableWidgetItem(str(data)))
+                col_index += 1
+        
+
+    def add_student(self):
+        firstname = self.ui.lineEdit_firstname.text()
+        lastname = self.ui.lineEdit_lastname.text()
+        class_id = self.ui.lineEdit_classid.text()
+
+        if not firstname or not lastname or not class_id:
+            QMessageBox.warning(self, "خطا", "لطفا همه فیلدها را پر نمائید")
+            return
+        self.db.execute('INSERT INTO students(first_name, last_name, class_id) VALUES (%s, %s, %s)',
+                        (firstname, lastname, class_id))
+        
+        self.load_students()
+        
         
