5261cdc96f7c2b8f1508113c6ef1636c2190cffb
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-11-18 18:17:39 +0330

    shop

--- javascript/calc/script.js
+++ javascript/calc/script.js
@@ -1,76 +1,15 @@
-let calculateHistory = []
-function add(a,b){
-    return a + b
-}
-function subtract(a,b){
-    return a - b
-}
-function multiply(a,b){
-    return a * b
-}
-function divide(a,b){
-    if(b === 0){
-        return "error zero division"
-    }
-    return a / b
-}
+let  products = [
+    {name: "shoe", price:85, inStock:true},
+    {name: "bag", price:96, inStock:false},
+    {name: "watch", price:60, inStock:true},
+]
 
-const factorial = (n) => {
-    if(n < 0) return " number is negative"
-    if (n === 0 || n === 1) return 1
-    let result = 1
-    for(let i = 2; i<=n; i++)
-        result *= i
-    return result
-}
+let availabeWithDiscount = products
+.filter(pr => pr.inStock)
+.map(pr => ({
+    name : pr.name,
+    originalPrice: pr.price,
+    discountedPrice :  pr.price * 0.9 
+}))
 
-function power(base , exponent){
-    return Math.pow(base, exponent)
-}
-function modulus(a , b){
-    return a % b
-}
-const sqrt = num => Math.sqrt(num)
-const square = num => num * num
-
-function calculateBasic(){
-    let basicNum1 = document.getElementById("basicNum1").value
-    let basicNum2 = document.getElementById("basicNum2").value
-    let op = document.getElementById("basicOp").value
-    let basicResult = document.getElementById("basicResult")
-    let result = 0
-    let opSymbol
-    if(op === "add"){
-        result = add(Number(basicNum1), Number(basicNum2))
-        opSymbol = "+"
-    }
-
-    basicResult.innerHTML = result
-    basicResult.classList.add("show")
-    addToHistory(`عملیات پایه : ${basicNum1} ${opSymbol} ${basicNum2} = ${result}`)
-}
-
-function addToHistory(calculation){
-    calculateHistory.unshift(calculation)
-    if(calculateHistory.length > 10){
-        calculateHistory.pop()
-    }
-    updateHistoryDisplay()
-}
-
-function updateHistoryDisplay(){
-    const historyList =  document.getElementById("historyList")
-    if(calculateHistory.length === 0){
-        historyList.innerHTML = '<p>هیچ تاریخچه ای وجود ندارد</p>'
-        return
-    }
-    historyList.innerHTML = calculateHistory.map(item => `<div>${item}</div>`).join('')
-    console.log(typeof historyList.innerHTML)
-}
-
-function clearHistory(){
-    calculateHistory = []
-
-    updateHistoryDisplay()
-
-}
+console.log(availabeWithDiscount)
