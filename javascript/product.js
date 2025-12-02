4654db4f00ca1937705b47be8c3bf9a72bb5bb20
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-12-02 18:46:33 +0330

    c

---
+++ javascript/product.js
@@ -0,0 +1,58 @@
+products =  []
+
+function addProduct(){
+    let productName = document.getElementById("productName").value;
+    let productCategory = document.getElementById("productCategory").value;
+    let productPrice = document.getElementById("productPrice").value;
+    let productStock = document.getElementById("productStock").value;
+    let productDesc = document.getElementById("productDesc").value;
+
+    let p = new Product(productName,
+         productCategory, productPrice, 
+         productStock, productDesc)
+                    products.push(p)
+    renderProducts()
+}
+
+
+function Product(name, category, price, stock, description){
+    this.id = Date.now()
+    this.name = name
+    this.category = category
+    this.price = price
+    this.stock = stock
+    this.description = description
+}
+
+function renderProducts(){
+    let grid =  document.getElementById("productsGrid")
+    if(products.length === 0){
+        grid.innerHTML = `
+            <div>
+                <div>
+                    <p>
+                        محصولی یافت نشد
+                    </p>
+                </div>
+            </div>
+        `
+        return
+    }
+    grid.innerHTML =  products.map(p => `
+            <div class="product-card">
+                <div class="product-name">${p.name}</div>
+                <div class="product-category">${p.category}</div>
+                <div class="product-price">${p.price}</div>
+                <div class="product-stock">${p.stock}</div>
+                <div >${p.description}</div>
+                <div class="product-actions">
+                <button class="btn-success">
+                افزایش
+                </button>
+
+                <button class="btn-danger">
+                حذف
+                </button>
+                </div>
+        `)
+}
