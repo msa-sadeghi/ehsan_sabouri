53ac788a88e447d1a47c1af5c1b353a8aeb89dce
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-12-16 18:10:17 +0330

    canvas html and css added

--- javascript/app.js
+++ javascript/app.js
@@ -1,10 +1,26 @@
-let button = document.querySelector("button")
-let div = document.querySelector("div")
+const nameInput = document.getElementById('name');
+const button = document.querySelector('button');
+const nameError = document.getElementById('nameError');
+const info = document.getElementById('info');
+
+button.addEventListener('click', (e) => {
+    e.preventDefault()
+    const name = nameInput.value.trim();
+    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!emailPattern.test(name)) {
+        nameError.style.display = 'block';
+        nameError.textContent = 'نام باید حداقل ۳ کاراکتر باشد.';
+    }else{
+        nameError.style.display = 'none';
+    }
+
+    let newDiv = document.createElement('div')
+    newDiv.innerHTML = `<h1>اسم:</h1>
+    
+    `
+
+    info.append(newDiv)
+    info.style.display = "block"
 
 
-div.addEventListener('click',  function(){
-    console.log("div clicked")
-},{capture:true})
-button.addEventListener('click',  function(){
-    console.log("button clicked")
 })
