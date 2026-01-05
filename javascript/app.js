a19b5cdb7d442d7bad4c68548ae5ec29bc89b9f7
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-01-05 19:59:03 +0330

    exercise

--- javascript/app.js
+++ javascript/app.js
@@ -1,35 +1,60 @@
-let currentnumber = 10
-let countdownElemet = document.getElementById('countdown')
-let interval = null
-function startCountdown(){
-    currentnumber = 10
-    countdownElemet.textContent = currentnumber
-    interval = setInterval(()=>{
-        currentnumber--
-        countdownElemet.textContent = currentnumber
-        if(currentnumber === 0){
-            clearInterval(interval)
-            setTimeout(()=>{
-                countdownElemet.textContent = "s"
-                setTimeout(()=>{
-                    
-                    countdownElemet.textContent = "10"
-                }, 2000)
-            }, 500)
-        }
-    }, 1000)
-}
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then(
+//     response => response.json()
+// )
+// .then(data => console.log(data))
+// .catch(error => console.log("error"))
 
-function stopCountdown(){
-    if(interval !== null){
-        clearInterval(interval)
-        interval = null
-        countdownElemet.textContent = "s"
-                setTimeout(()=>{
-                    
-                    countdownElemet.textContent = "10"
-                    currentnumber = 10
-                }, 2000)
 
+
+async function  getUsers(url) {
+    try{
+        const response = await fetch(url)
+    if(!response.ok){
+        throw new Error("er")
+    }
+    const users =  await response.json()
+
+    console.log(users)
+    }
+    catch(error){
+        console.log(error.message)
     }
 }
+
+getUsers('https://jsonplaceholder.typicode.com/users')
+
+async function createPost() {
+    
+    const newPost = {
+        title:'test post',
+        body:'this is a test post',
+        userId:1
+    }
+
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts'
+        ,{
+            method:'POST',
+            headers :{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(newPost)
+        }
+    )
+    const data = await response.json()
+    console.log(data)
+}
+
+createPost()
+
+// تمرین ۱
+
+// یک تابع بنویس:
+
+// لیست پست‌ها را بگیرد
+
+// فقط عنوان‌ها را چاپ کند
+
+// تمرین ۲
+
+// یک پست با عنوان دلخواه خودت ارسال کن
