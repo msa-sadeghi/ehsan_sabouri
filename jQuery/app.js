dc7453a5d09476db19e451c3744b11be71e7a5b1
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-02-10 18:43:11 +0330

    codes

--- jQuery/app.js
+++ jQuery/app.js
@@ -1,11 +1,21 @@
-$(function(){
-   $('#showBtn').click(function(){
-    $('#message-box').show(500)
-   })
-   $('#hideBtn').click(function(){
-    $('#message-box').hide(500)
-   })
-   $('#toggleBtn').click(function(){
-    $('#message-box').toggle(500)
-   })
-})
+$(function() {
+            // فرزندان مستقیم - فقط p هایی که مستقیماً داخل .container هستند
+            $('#directChild').click(function() {
+                $('.container > p').css('background-color', 'yellow');
+            });
+            
+            // تمام فرزندان - تمام p ها در هر عمقی
+            $('#allDesc').click(function() {
+                $('.container p').css('border', '2px solid red');
+            });
+            
+            // عنصر مجاور - فقط اولین p بعد از h2
+            $('#adjacent').click(function() {
+                $('h2 + p').css('color', 'blue');
+            });
+            
+            // تمام همسطح‌های بعدی
+            $('#siblings').click(function() {
+                $('h2 ~ p').css('font-weight', 'bold');
+            });
+        });
