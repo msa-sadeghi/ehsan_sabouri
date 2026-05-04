2e8b24f22539ace2289e805387df86dee9d7e06b
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2026-05-04 19:07:24 +0330

    s

--- react/my-shop/online-shop-backend/routes.py
+++ react/my-shop/online-shop-backend/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 from models import Product
-
+from extensions import db
+from utils import error_response
 
 products_routes = Blueprint("products", __name__)
 
@@ -9,3 +10,62 @@ products_routes = Blueprint("products", __name__)
 def get_products():
     products = Product.query.all()
     return jsonify([p.to_dict() for p in products])
+
+
+@products_routes.route("/products", methods=["POST"])
+def create_product():
+    if not request.is_json:
+        return error_response("Content-Type must be application/json", 415)
+    data = request.json
+
+    required_fields = [
+        "name",
+        "description",
+        "price",
+    ]
+    for field in required_fields:
+        if field not in data:
+            return error_response(f"{field} is required")
+
+    if not isinstance(data.get("price"), (int, float)):
+        return error_response("price must be a number")
+
+    p = Product(
+        name=data["name"],
+        description=data["description"],
+        price=data["price"],
+        image=data.get("image"),
+        in_stock=data.get("in_stock", True),
+    )
+
+    db.session.add(p)
+    db.session.commit()
+
+    return jsonify(p.to_dict()), 201
+
+
+@products_routes.route("/products/<int:id>", methods=["GET"])
+def get_product(id):
+    p = Product.query.get_or_404(id)
+    return jsonify(p.to_dict())
+
+
+@products_routes.route("/products/<int:id>", methods=["PUT"])
+def update_product(id):
+    p = Product.query.get_or_404(id)
+    data = request.get_json()
+    p.description = data.get("description", p.description)
+    p.price = data.get("price", p.price)
+    p.image = data.get("image", p.image)
+    p.name = data.get("name", p.name)
+    p.in_stock = data.get("in_stock", p.in_stock)
+    db.session.commit()
+    return jsonify(p.to_dict())
+
+
+@products_routes.route("/products/<int:id>", methods=["DELETE"])
+def delete_product(id):
+    p = Product.query.get_or_404(id)
+    db.session.delete(p)
+    db.session.commit()
+    return jsonify({"message": "Product deleted successfult", "id": id})
